33733da8f25567d49d7c0e253ed0d17e35b0a858
Brandonlrs27 <43393536+Brandonlrs27@users.noreply.github.com>
2018-12-10 18:42:15 -0800

    Add files via upload

---
+++ BusquedaHash/BusquedaHash/Hash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace BusquedaHash
+{
+    public class Hash
+    {
+        Hashtable datos = new Hashtable(); //Crea un hashtable para agregar datos
+        public void Buscar()
+        {
+            Console.WriteLine("Datos disponibles: ");
+            datos.Add(123, "Brandon"); //Los datos contienen una llave unica y el dato
+            datos.Add(132, "Carlos");//La clave no puede repetirse
+            datos.Add(321, "Jose");
+            datos.Add(213, "David");
+            datos.Add(232, "Chuleta");
+            Imprimir(datos); //Se llama el metodo imprimir
+            Console.WriteLine();
+            Console.WriteLine("Introduzca clave para buscar dato: "); //Se pregunta y lee la clave 
+            int clave = int.Parse(Console.ReadLine()); //Se guarda en la variable
+            if (datos.ContainsKey(clave)) //Si datos contiene la llave que esta en la variable
+            {
+                Console.WriteLine("Se encontro y contiene el dato: {0}", datos[clave]); //Escribira el dato encontrado con esa llave
+            }
+            else
+            {
+                Console.WriteLine("Dato no encontrado"); //Si no es encontrado, mensaje de que no
+            }
+
+        }
+        public void Imprimir(Hashtable hashtable) //Metodo imprimir con parametro Hashtable
+        {
+            foreach (DictionaryEntry i in datos) //Aqui se busca un dictonaryentry para imprimir, osea que tenga la clave algun dato cualquiera
+            {
+                Console.WriteLine("{0}-{1}", i.Key, i.Value);
+            }
+        }
+
+    }
+}
+
---
+++ BusquedaHash/BusquedaHash/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaHash
+{
+    class Program
+    { //Lares Dominguez Brandon - 17211532
+        static void Main(string[] args)
+        {
+            Hash Busquedahash = new Hash();
+            Busquedahash.Buscar();
+            Console.ReadKey();
+        }
+    }
+}
